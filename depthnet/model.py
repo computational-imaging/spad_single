5b5fb721b8de23fdd7b98c5c77292619352a323c
Mark Nishimura <markn1@stanford.edu>
2018-10-15 22:50:17 -0700

    Switching off jupyter notebook

--- depthnet/model.py
+++ depthnet/model.py
@@ -20,11 +20,10 @@ def create_sublayer_dict(input_nc, output_nc, layer_index, nconvs, norm_layer, *
     return sublayer
 
 class DepthNet(nn.Module):
-    def __init__(self, input_nc, output_nc, norm_layer=nn.BatchNorm2d):
+    def __init__(self, input_nc, output_nc, norm_layer=nn.BatchNorm2d, use_bias=True):
         super(DepthNet, self).__init__()
         self.input_nc = input_nc
         self.output_nc = output_nc
-        use_bias = True
 
         # Conv1
         model1 = create_sublayer_dict(input_nc, 32, 1, 2, norm_layer, 
@@ -121,8 +120,9 @@ class DepthNet(nn.Module):
         self.model1short10 = nn.Sequential(model1short10)
         self.model_out = nn.Sequential(model_out)
 
-    def forward(self, input_A):
-        conv1_2 = self.model1(input_A)
+    def forward(self, input_):
+        rgb = input_["rgb"]
+        conv1_2 = self.model1(rgb)
         conv2_2 = self.model2(conv1_2[:,:,::2,::2]) # downsample
         conv3_3 = self.model3(conv2_2[:,:,::2,::2]) # downsample
         conv4_3 = self.model4(conv3_3[:,:,::2,::2]) # downsample
@@ -139,3 +139,67 @@ class DepthNet(nn.Module):
 
         return out_reg
 
+class DepthNetWithHints(nn.Module):
+    def __init__(self, depthnet, hist_len, num_hints_layers):
+        """Takes an existing DepthNet, along with the size of the
+        histogram and the size of its bins"""
+        super(DepthNetWithHints, self).__init__()
+        self.input_nc = depthnet.input_nc
+        self.output_nc = depthnet.output_nc
+        self.depthnet = depthnet
+        self.hist_len = hist_len
+        self.num_hints_layers = num_hints_layers
+        
+        # Create hints network
+        assert num_hints_layers > 0
+        # Extract number of out channels of conv4
+        hints_output = depthnet.model4[0].out_channels
+        hints = OrderedDict([("hints_conv_0", nn.Conv2d(self.hist_len, hints_output, kernel_size=1))])
+        hints.update({"hints_relu0_1": nn.ReLU(True)})
+        j = 2
+        for _ in range(num_hints_layers-1):
+            hints.update({"hints_conv_{}".format(j): nn.Conv2d(hints_output, hints_output, kernel_size=1)})
+            j += 1
+            hints.update({"hints_relu_{}".format(j): nn.ReLU(True)})
+            j += 1
+            
+        self.global_hints = nn.Sequential(hints)
+            
+        def forward(self, input_):
+            # |hist| should be a (1, hist_len, 1, 1) tensor
+            # First 4 layers of regular depthnet
+            rgb = input_["rgb"]
+            hist = input_["hist"]
+            conv1_2 = self.depthnet.model1(rgb)
+            conv2_2 = self.depthnet.model2(conv1_2[:,:,::2,::2]) # downsample
+            conv3_3 = self.depthnet.model3(conv2_2[:,:,::2,::2]) # downsample
+            conv4_3 = self.depthnet.model4(conv3_3[:,:,::2,::2]) # downsample
+            
+            # Global hints network
+            hints_out = self.global_hints(hist)
+            # Replicate and add to output of conv4 (broadcasting takes care of this)
+            conv5_3 = self.depthnet.model5(conv4_3 + hints_out)
+            
+            # Finish doing the rest of the depthnet
+            conv6_3 = self.depthnet.model6(conv5_3)
+            conv7_3 = self.depthnet.model7(conv6_3)
+            conv8_up = self.depthnet.model8up(conv7_3) + self.depthnet.model3short8(conv3_3) # Shortcut
+            conv8_3 = self.depthnet.model8(conv8_up)
+            conv9_up = self.depthnet.model9up(conv8_3) + self.depthnet.model2short9(conv2_2) # Shortcut
+            conv9_3 = self.depthnet.model9(conv9_up)
+            conv10_up = self.depthnet.model10up(conv9_3) + self.depthnet.model1short10(conv1_2) # Shortcut
+            conv10_2 = self.depthnet.model10(conv10_up)
+            out_reg = self.depthnet.model_out(conv10_2)
+            return out_reg
+    
+##################
+# Loss functions #
+##################
+
+def berhu(prediction, target):
+    diff = prediction - target
+    threshold = 0.2*torch.max(torch.abs(prediction - target))
+    c = threshold.detach()
+    l2_part = torch.sum((diff**2 + c**2))/(2*c)
+    l1_part = torch.sum(torch.abs(diff))
+    return l1_part+l2_part
---
+++ depthnet/train_utils.py
@@ -0,0 +1,166 @@
+# Set up training.
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+import torchvision.utils as vutils
+
+from depthnet.model import DepthNet
+import depthnet.utils as u
+
+def setup_training(opt, writer=None):
+    # Build model and loss
+    # Hyperparameters
+
+    model = DepthNet(opt.input_nc, opt.output_nc)
+    if torch.cuda.is_available():
+        model.cuda()
+    # Split parameters into weight and bias terms (different regularization)
+    params = [{"params": []}, {"params": [], "weight_decay": 0.0}]
+    for name, param in model.named_parameters():
+        if "weight" in name:
+            params[0]["params"].append(param)
+        elif "bias" in name:
+            params[1]["params"].append(param)
+        else:
+            raise ValueError("Unknown param type: {}".format(name))
+
+    #print(params)
+    
+    # Checkpointing
+    if opt.checkpoint is not None:
+        if torch.cuda.is_available():
+            checkpoint = torch.load(opt.checkpoint)
+        else:
+            # Load GPU model on CPU
+            checkpoint = torch.load(opt.checkpoint,
+                                    map_location=lambda storage,
+                                    loc: storage)
+        start_epoch = checkpoint['epoch'] + 1
+        best_loss = checkpoint['best_loss']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer = optim.Adam(params, lr=opt.lr, weight_decay=opt.weight_decay)
+        optimizer.load_state_dict(checkpoint['optim_state_dict'])
+        trainlosses = checkpoint['trainlosses']
+        global_it = len(trainlosses)
+        vallosses = checkpoint['vallosses']
+        if writer is not None:
+            for i, trainloss in enumerate(trainlosses): # For tensorboardx
+                writer.add_scalar("data/trainloss", trainloss, i)
+            for i, valloss in enumerate(vallosses): # For tensorboardx
+                writer.add_scalar("data/valloss", valloss, i)
+        global_it = len(trainlosses)
+        print("=> loaded checkpoint '{}' (trained for {} epoch(s)).".format(opt.checkpoint, start_epoch))
+    else:
+        start_epoch = 0
+        global_it = 0 # Track global iterations
+        best_loss = torch.FloatTensor([float('inf')])
+        optimizer = optim.Adam(params, lr=opt.lr, weight_decay=opt.weight_decay)
+        trainlosses = []
+        vallosses = []
+        # Initialize weights:
+        for name, param in model.named_parameters():
+            if "conv" in name and "weight" in name:
+    #             print(name)
+                nn.init.xavier_normal_(param)
+            if "norm" in name and "weight" in name:
+    #             print(name)
+                nn.init.constant_(param, 1)
+            elif "bias" in name:
+                nn.init.constant_(param, 0)
+
+    # Scheduler
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer,
+                                               milestones=opt.milestones,
+                                               gamma=opt.gamma)
+    scheduler.last_epoch = start_epoch - 1
+
+    # Print summary of setup:
+    print("loaded opt.checkpoint: {}".format(opt.checkpoint))
+    print("start_epoch: {}".format(start_epoch))
+    print("global_it: {}".format(global_it))
+    print("optimizer: {}".format(optimizer))
+    print("batch_size: {}".format(opt.batch_size))
+    print("num_epochs: {}".format(opt.num_epochs))
+    print("learning rate (initial): {}".format(scheduler.get_lr()))
+    print("scheduler: {}".format(scheduler.state_dict()))
+    
+    out = {"model": model,
+           "start_epoch": start_epoch,
+           "num_epochs": opt.num_epochs,
+           "global_it": global_it,
+           "scheduler": scheduler,
+           "trainlosses": trainlosses,
+           "vallosses": vallosses
+          }
+    return out
+
+####################
+# Run the training #
+####################
+def train(opt,
+          model, loss,
+          start_epoch, num_epochs,
+          trainlosses, vallosses,
+          global_it, scheduler, trainLoader, valLoader=None,
+          test_run=False, writer=None):
+    for epoch in range(start_epoch, start_epoch + num_epochs):
+        print("epoch: {}".format(epoch))
+        data = None
+        output = None
+        for it, data in enumerate(trainLoader):
+            input_ = {}
+            for key in data:
+                input_[key] = data[key].float()
+                if torch.cuda.is_available():
+                    input_[key] = input_[key].cuda()
+
+            depth = input_["depth"]
+            # New batch
+            scheduler.optimizer.zero_grad()
+            output = model(input_)
+            trainloss = loss(output, depth)
+            trainloss.backward()
+            scheduler.optimizer.step()
+            global_it += 1
+
+            if not (it % 10):
+                print("\titeration: {}\ttrain loss: {}".format(it, trainloss.item()))
+            trainlosses.append(trainloss.item())
+            writer.add_scalar("data/trainloss", trainloss.item(), global_it)
+            if test_run: # Stop after 5 batches
+                if not ((it + 1) % 5):
+                    break
+        # Checkpointing
+        if valLoader is not None:
+            valloss = u.validate(loss, model, valLoader)
+            print("End epoch {}\tval loss: {}".format(epoch, valloss))
+            vallosses.append(valloss)
+            writer.add_scalar("data/valloss", valloss, epoch)
+
+        # Save the last batch output of every epoch
+        rgb_input = vutils.make_grid(data["rgb"], nrow=opt.batch_size, normalize=True, scale_each=True)
+        writer.add_image('image/rgb_input', rgb_input, epoch)
+
+        depth_truth = vutils.make_grid(data["depth"], nrow=opt.batch_size, normalize=True, scale_each=True)
+        writer.add_image('image/depth_truth', depth_truth, epoch)
+
+        depth_output = vutils.make_grid(output, nrow=opt.batch_size, normalize=True, scale_each=True)
+        writer.add_image('image/depth_output', depth_output, epoch)
+
+        for name, param in model.named_parameters():
+            writer.add_histogram(name, param.clone().cpu().data.numpy(), global_it)
+    #     save_images(data["rgb"], data["depth"], output, outputDir="images", filename="epoch_{}".format(epoch))
+
+#        is_best = bool(trainloss.data.cpu().numpy() < best_loss.numpy())
+        # Get greater Tensor to keep track best acc
+#        best_loss = torch.FloatTensor(min(trainloss.data.cpu().numpy(), best_loss.numpy()))
+        # Save checkpoint
+        u.save_checkpoint({
+            'epoch': epoch,
+            'state_dict': model.state_dict(),
+#            'best_loss': best_loss,
+            'optim_state_dict': scheduler.optimizer.state_dict(),
+            'trainlosses': trainlosses,
+            'vallosses': vallosses
+        }, False, filename="checkpoints/checkpoint_epoch_{}.pth.tar".format(epoch), always_save=True)
