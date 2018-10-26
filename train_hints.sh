7e3bb4d7785b07197712e78d9e442ffa7cd45e46
Mark Nishimura <markn1@stanford.edu>
2018-10-26 07:09:40 -0700

    Cleaning up.

--- train_hints.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-python train.py depth_hints data/sunrgbd_all/train.txt data/sunrgbd_all --valFile data/sunrgbd_all/val.txt --valDir data/sunrgbd_all --milestones 25 --num-epochs 50 --cuda-device 1
