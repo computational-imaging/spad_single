af097029045c0028bdfcc4cb3e60b988acb63353
Mark Nishimura <markn1@stanford.edu>
2018-10-26 07:09:40 -0700

    Cleaning up.

--- fullrun.sh
+++
@@ -1,1 +0,0 @@
-nohup python train.py with 'cuda_device="2"' 'num_epochs=20' 'milestones=[5, 10]' 'comment="_fullrun"' -F experiments &
--- train_depth.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-nohup python train.py depth data/sunrgbd_nyu/train.txt data/sunrgbd_nyu --valFile data/sunrgbd_nyu/dev.txt --valDir data/sunrgbd_nyu --milestones 25 --num-epochs 50 &
-
-
