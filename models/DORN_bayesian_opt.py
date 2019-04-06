e4cc89cd197dcce2d1d81bb5353638e5c11794f1
Mark <nishimuramarky@yahoo.com>
2019-04-05 17:11:36 -0700

    SGD method converges under no-albedo, no-falloff, no-noise conditions.

--- models/DORN_bayesian_opt.py
+++ models/DORN_bayesian_opt.py
@@ -4,7 +4,9 @@ from torch.nn import MSELoss
 import numpy as np
 from models.data.utils.sid_utils import SIDTorch
 from torch.optim import SGD
-
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
 import os
 
 # from models.core.model_core import Model
@@ -16,7 +18,8 @@ class DORN_bayesian_opt:
     Performs SGD to optimize the depth map further after being given an initial depth map
     estimate from DORN.
     """
-    def __init__(self, sgd_iters=1, use_albedo=True, use_squared_falloff=True, lr=1e-3, hints_len=68, spad_weight=1.,
+    def __init__(self, sgd_iters=1, use_albedo=True, use_squared_falloff=True, normalize_to=1000.,
+                 lr=1e-3, hints_len=68, spad_weight=1.,
                  in_channels=3, in_height=257, in_width=353,
                  sid_bins=68, offset=0.,
                  min_depth=0., max_depth=10.,
@@ -30,6 +33,7 @@ class DORN_bayesian_opt:
         self.sgd_iters = sgd_iters
         self.use_albedo = use_albedo
         self.use_squared_falloff = use_squared_falloff
+        self.normalize_to = normalize_to
         self.lr = lr
         self.loss = MSELoss()
         # self.loss.to(device)
@@ -62,25 +66,50 @@ class DORN_bayesian_opt:
             log_probs, _ = self.to_logprobs(x)
             depth_index = torch.sum((log_probs >= np.log(0.5)), dim=1, keepdim=True).long()
             # print(depth_index[:,:,3,3])
-            depth_hist = torch.zeros_like(log_probs, device=device)
-            depth_hist.scatter_(1, depth_index, 1)
+            depth = torch.zeros_like(log_probs, device=device)
+            depth.scatter_(dim=1, index=depth_index, value=1)
             # print(depth_hist[:,:,3,3])
-        return depth_hist
+            # Start with log probs, add a small epsilon to prevent -inf
+            # depth_hist
+        return depth
 
     def optimize_depth_map(self, depth_hist, input_, device):
         """Perform SGD on the initial input to get the refined input"""
         gt_hist = input_["spad"].to(device)
         albedo = input_["albedo"].to(device)
         N, C, W, H = depth_hist.size()
+        gt_hist *= W*H/torch.sum(gt_hist)
+
+        plt.figure()
+        plt.plot(gt_hist.clone().squeeze().detach().cpu().numpy())
+        plt.title("spad")
+        plt.draw()
+        plt.pause(0.001)
 
         albedo = albedo[:,1,:,:].requires_grad_(False)
         output = depth_hist.clone().detach().requires_grad_(True)
+        print(output.dtype)
         optimizer = SGD([output], lr=self.lr)
         # self.zero_pad = torch.zeros((N, 1, W, H), requires_grad=False).to(device)
         self.one_over_depth_squared = self.one_over_depth_squared.to(device)
         # self.zero_pad.requires_grad = False
         for it in range(self.sgd_iters):
             output_hist = self.spad_forward(output, albedo)
+
+            if not it % 10:
+                plt.figure()
+                output_spad = output_hist/torch.sum(output_hist)
+                plt.plot(output_spad.squeeze().clone().detach().cpu().numpy())
+                plt.title("SPAD at iteration {}".format(it))
+                plt.draw()
+                plt.pause(0.001)
+
+            # plt.figure()
+            # plt.plot(output[:,:,130,130].squeeze().clone().detach().cpu().numpy())
+            # plt.title("(130, 130) histogram at step {}".format(it))
+            # plt.draw()
+            # plt.pause(0.001)
+
             loss_val = self.loss(gt_hist, output_hist)
             optimizer.zero_grad()
             loss_val.backward()
@@ -108,7 +137,8 @@ class DORN_bayesian_opt:
         if self.use_squared_falloff:
             weights *= one_over_depth_squared_unsqueezed
         simulated_spad = torch.sum(weights, dim=(2, 3), keepdim=True)
-        simulated_spad /= torch.sum(simulated_spad)
+        print("simulated scale factor: {}".format(W*H/torch.sum(simulated_spad)))
+        simulated_spad *= W*H/torch.sum(simulated_spad)
         return simulated_spad
 
     def evaluate(self, data, device):
@@ -120,6 +150,7 @@ class DORN_bayesian_opt:
 
 DORN_bayesian_opt.to_logprobs = staticmethod(DORN_nyu_nohints.to_logprobs)
 DORN_bayesian_opt.ord_decode = staticmethod(DORN_nyu_nohints.ord_decode)
+DORN_bayesian_opt.get_metrics = staticmethod(DORN_nyu_nohints.get_metrics)
 
 
 if __name__ == "__main__":
@@ -129,7 +160,10 @@ if __name__ == "__main__":
     from models.data.nyuv2_official_hints_sid_dataset import load_data, cfg
     from models.data.utils.spad_utils import cfg as spad_cfg
     import torchvision.utils as vutils
+    import matplotlib
+    matplotlib.use("TKAgg")
     import matplotlib.pyplot as plt
+
     data_config = cfg()
     # print(data_config)
     # data_config["normalization"] = "dorn"
@@ -141,26 +175,70 @@ if __name__ == "__main__":
     # print(spad_config)
     del data_config["data_name"]
     device = torch.device("cuda")
-    _, val, _ = load_data(**data_config, spad_config=spad_config)
-    input_ = val[1]
-    for key in ["rgb", "albedo", "spad", "mask"]:
+    _, val, test = load_data(**data_config, spad_config=spad_config)
+    # input_ = val[1]
+    input_ = test.get_item_by_id("living_room_0059/1591")
+    for key in ["rgb", "albedo", "rawdepth", "spad", "mask"]:
         input_[key] = input_[key].unsqueeze(0)
     print(input_["entry"])
 
-    for sgd_iters in range(5):
-        print("Running with {} iters of SGD...".format(sgd_iters))
-        model = DORN_bayesian_opt(sgd_iters=sgd_iters, use_albedo=spad_config["use_albedo"],
-                                  use_squared_falloff=spad_config["use_squared_falloff"], lr=1.)
-        model.feature_extractor.to(device)
-        depth_hist = model.initialize(input_, device)
-        if sgd_iters == 0:
-            print(depth_hist[:,:,30,30])
-        output = model.optimize_depth_map(depth_hist, input_, device)
-        print(output[:,:,30,30])
-        output_dir = "."
-        outfile = os.path.join(output_dir, "{}_pred_{}.png".format(input_["entry"].replace("/", "_"), sgd_iters))
+    # sgd_iters = 400
+    sgd_iters = 100
+    model = DORN_bayesian_opt(sgd_iters=sgd_iters, use_albedo=spad_config["use_albedo"],
+                              use_squared_falloff=spad_config["use_squared_falloff"], lr=1e-4)
+    model.feature_extractor.to(device)
+
+
+
+    plt.figure()
+    depth_truth_img = vutils.make_grid(input_["rawdepth"], normalize=True, range=(model.min_depth, model.max_depth))
+    plt.imshow(depth_truth_img.numpy().transpose(1, 2, 0))
+    plt.title("depth_truth")
+    plt.draw()
+    plt.pause(0.001)
+
+    # plt.figure()
+    # albedo_img = vutils.make_grid(input_["albedo"][:,1,:,:])
+    # plt.title("albedo")
+    # plt.imshow(albedo_img.numpy().transpose(1, 2, 0)/255.)
+    # plt.draw()
+    # plt.pause(0.001)
+
+    # for sgd_iters in range(5):
+    print("Running with {} iters of SGD...".format(sgd_iters))
+
+    depth = model.initialize(input_, device)
+    depth_optimized = model.optimize_depth_map(depth, input_, device)
+
+    # Decode the output using usual ordinal decoding method.
+    # Normalize each pixel histogram
+    depth_optimized = depth_optimized / torch.sum(depth_optimized, dim=1, keepdim=True)
+
+    plt.figure()
+    plt.plot(depth_optimized[:,:,130,130].squeeze().clone().detach().cpu().numpy())
+    plt.title("(130, 130) histogram at end")
+    plt.draw()
+    plt.pause(0.001)
+
+    logprobs = torch.log(1. - torch.cumsum(depth_optimized, dim=1))
+    depth_pred = model.ord_decode((logprobs,None), model.sid_obj)
+    depth_img = vutils.make_grid(depth_pred, normalize=True, range=(model.min_depth, model.max_depth))
+
+    plt.figure()
+    plt.imshow(depth_img.numpy().transpose(1, 2, 0))
+    plt.title("Predicted depth")
+    plt.draw()
+    plt.pause(0.001)
+
+    # print(depth_pred.shape)
+    # print(input_["rawdepth"].shape)
+    # print(input_["mask"].shape)
+    print(model.get_metrics(depth_pred, input_["rawdepth"], input_["mask"]))
+    # print(output[:,:,30,30])
+    # output_dir = "."
+    # outfile = os.path.join(output_dir, "{}_pred_{}.png".format(input_["entry"].replace("/", "_"), sgd_iters))
         # output_depth = model.ord_decode((output_depth_probs, None), model.sid_obj)
         # # print(output_depth)
         #
         # vutils.save_image(output_depth, outfile, nrow=1, normalize=True, range=(model.min_depth, model.max_depth))
-
+    input("Press the enter key to exit.")
