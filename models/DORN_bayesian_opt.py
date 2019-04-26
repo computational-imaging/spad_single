9d2d26bcf220771fea63045820d1ab4d8cd47d76
Mark <nishimuramarky@yahoo.com>
2019-04-25 19:16:12 -0700

    Working sinkhorn optimize_depth_map.

--- models/DORN_bayesian_opt.py
+++ models/DORN_bayesian_opt.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.nn import MSELoss
 import numpy as np
 from models.data.utils.sid_utils import SIDTorch
 from torch.optim import SGD
 import matplotlib
-matplotlib.use("TKAgg")
-import matplotlib.pyplot as plt
+# matplotlib.use("TKAgg")
+# import matplotlib.pyplot as plt
 import os
 
 # from models.core.model_core import Model
@@ -67,17 +68,23 @@ class DORN_bayesian_opt:
             depth_index = torch.sum((log_probs >= np.log(0.5)), dim=1, keepdim=True).long()
             # print(depth_index[:,:,3,3])
             depth = torch.zeros_like(log_probs, device=device)
-            depth.scatter_(dim=1, index=depth_index, value=1)
-            # print(depth_hist[:,:,3,3])
-            # Start with log probs, add a small epsilon to prevent -inf
-            # depth_hist
-        return depth
 
-    def optimize_depth_map(self, depth_hist, input_, device):
+            # Add base level of uncertainty
+            # base = 0.8
+            # depth.add_(base)
+
+            depth.scatter_(dim=1, index=depth_index, value=1)
+
+            # Base level
+            print(depth[:,:,1,1])
+        return depth, log_probs, depth_index
+
+    # def optimize_depth_map(self, depth_hist, input_, device):
+    def optimize_depth_map(self, depth_index, input_, device):
         """Perform SGD on the initial input to get the refined input"""
         gt_hist = input_["spad"].to(device)
         albedo = input_["albedo"].to(device)
-        N, C, W, H = depth_hist.size()
+        _, _, W, H = depth_index.size()
         gt_hist *= W*H/torch.sum(gt_hist)
 
         plt.figure()
@@ -87,7 +94,11 @@ class DORN_bayesian_opt:
         plt.pause(0.001)
 
         albedo = albedo[:,1,:,:].requires_grad_(False)
-        output = depth_hist.clone().detach().requires_grad_(True)
+        # with torch.no_grad():
+        #     depth_hist = torch.log(depth_hist)
+        output = depth_index.clone().detach().float().requires_grad_(True).to(device)
+        depth_bin_indices = torch.Tensor(range(self.sid_bins)).unsqueeze(-1).unsqueeze(-1).unsqueeze(0).to(device)
+        # sid_bin_values = self.sid_obj.sid_bin_values.to(device)[:68]
         # print(output.dtype)
         optimizer = SGD([output], lr=self.lr)
         # self.zero_pad = torch.zeros((N, 1, W, H), requires_grad=False).to(device)
@@ -96,10 +107,18 @@ class DORN_bayesian_opt:
         for it in range(self.sgd_iters):
             # Renormalize each output pixel's histogram
             # with torch.no_grad():
-            output = output / torch.sum(output, dim=1, keepdim=True)
+            # output = output / torch.sum(output, dim=1, keepdim=True)
+            # output_probs = torch.exp(output)
+            # output_probs = self.depth_to_hist(output, depth_bin_indices)
             output_hist = self.spad_forward(output, albedo)
 
-            if not it % 10:
+            if not it % 100:
+                # plt.figure()
+                # plt.plot(output_probs[:, :, 130, 130].squeeze().clone().detach().cpu().numpy())
+                # plt.title("(130, 130) output_probs at iteration {}".format(it))
+                # plt.draw()
+                # plt.pause(0.001)
+
                 plt.figure()
                 output_spad = output_hist/torch.sum(output_hist)
                 plt.plot(output_spad.squeeze().clone().detach().cpu().numpy())
@@ -114,9 +133,10 @@ class DORN_bayesian_opt:
             # plt.pause(0.001)
 
             loss_val = self.loss(gt_hist, output_hist)
+            print("loss_val", loss_val)
             optimizer.zero_grad()
             loss_val.backward()
-            print(torch.norm(output.grad))
+            print("output_grad", torch.norm(output.grad))
             optimizer.step()
         return output
 
@@ -144,6 +164,21 @@ class DORN_bayesian_opt:
         simulated_spad *= W*H/torch.sum(simulated_spad)
         return simulated_spad
 
+    @staticmethod
+    def depth_to_hist(depth_index, depth_bin_indices):
+        """Takes a depth map with indices corresponding to depth bins and outputs an
+        approximate histogram from, differentiably.
+        """
+        # N, C, W, H = depth_index.size()
+        # print(depth_index.dtype)
+
+        # print(depth_index[:,:,130,130])
+        diff = depth_index - depth_bin_indices
+        # print(diff[:,:,130,130])
+        depth_hist = F.softmax(1./(diff**2 + 1e-4), dim=1)
+        return depth_hist
+
+
     def evaluate(self, data, device):
         depth_hist = model.initialize(data, device)
         output_depth_probs = model.optimize_depth_map(depth_hist, data, device)
@@ -160,7 +195,9 @@ if __name__ == "__main__":
     import os
     import numpy as np
     from torch.utils.data import DataLoader
+    from models.data.utils.sid_utils import SID
     from models.data.nyuv2_official_hints_sid_dataset import load_data, cfg
+    from models.data.utils.spad_utils import SimulateSpad
     from models.data.utils.spad_utils import cfg as spad_cfg
     import torchvision.utils as vutils
     import matplotlib
@@ -186,9 +223,9 @@ if __name__ == "__main__":
     print(input_["entry"])
 
     # sgd_iters = 400
-    sgd_iters = 100
+    sgd_iters = 300
     model = DORN_bayesian_opt(sgd_iters=sgd_iters, use_albedo=spad_config["use_albedo"],
-                              use_squared_falloff=spad_config["use_squared_falloff"], lr=1e-4)
+                              use_squared_falloff=spad_config["use_squared_falloff"], lr=1e-3)
     model.feature_extractor.to(device)
 
 
@@ -210,26 +247,63 @@ if __name__ == "__main__":
     # for sgd_iters in range(5):
     print("Running with {} iters of SGD...".format(sgd_iters))
 
-    depth = model.initialize(input_, device)
-    depth_optimized = model.optimize_depth_map(depth, input_, device)
+    depth, log_probs_dorn, depth_index = model.initialize(input_, device)
+    # log_probs_optimized = model.optimize_depth_map(depth, input_, device)
+    # depth_optimized = model.optimize_depth_map(depth, input_, device)
+    depth_optimized_index = model.optimize_depth_map(depth_index, input_, device)
 
     # Decode the output using usual ordinal decoding method.
-    # Normalize each pixel histogram
-    depth_optimized = depth_optimized / torch.sum(depth_optimized, dim=1, keepdim=True)
+    # log_probs_optimized = torch.log(1. - torch.cumsum(F.softmax(log_probs_optimized, dim=1), dim=1))
+    #
+    # plt.figure()
+    # plt.plot(log_probs_optimized[:,:,130,130].squeeze().clone().detach().cpu().numpy())
+    # plt.title("(130, 130) optimized log-histogram")
+    # plt.draw()
+    # plt.pause(0.001)
 
-    plt.figure()
-    plt.plot(depth_optimized[:,:,130,130].squeeze().clone().detach().cpu().numpy())
-    plt.title("(130, 130) histogram at end")
-    plt.draw()
-    plt.pause(0.001)
+    # plt.figure()
+    # plt.plot(log_probs_dorn[:, :, 130, 130].squeeze().clone().detach().cpu().numpy())
+    # plt.title("(130, 130) DORN log-histogram")
+    # plt.draw()
+    # plt.pause(0.001)
 
-    logprobs = torch.log(1. - torch.cumsum(depth_optimized, dim=1))
-    depth_pred = model.ord_decode((logprobs,None), model.sid_obj)
+    # log_probs = log_probs_dorn + 0.3*log_probs_optimized
+    # depth_pred = model.ord_decode((log_probs,None), model.sid_obj)
+
+    # Decode by rounding the output index and taking from the sid_obj
+    depth_optimized_index_rounded = depth_optimized_index.round().detach().cpu().long()
+    depth_pred = model.sid_obj.get_value_from_sid_index(depth_optimized_index_rounded)
     depth_img = vutils.make_grid(depth_pred, normalize=True, range=(model.min_depth, model.max_depth))
 
     plt.figure()
     plt.imshow(depth_img.numpy().transpose(1, 2, 0))
     plt.title("Predicted depth")
+    plt.draw()
+    plt.pause(0.001)
+
+    # Get output histogram as well
+    # print(test.transform[6])
+    depth_img_np = depth_img.numpy()
+    simulate_spad_transform = SimulateSpad("rawdepth", "albedo", "mask", "spad", data_config["min_depth"], data_config["max_depth"],
+                                           spad_config["spad_bins"],
+                                           spad_config["photon_count"],
+                                           spad_config["dc_count"],
+                                           spad_config["fwhm_ps"],
+                                           spad_config["use_albedo"],
+                                           spad_config["use_squared_falloff"],
+                                           sid_obj=SID(data_config["sid_bins"], data_config["alpha"],
+                                                       data_config["beta"], data_config["offset"]))
+    print(depth_pred.shape)
+    print(input_["albedo"].shape)
+    print(input_["mask"].shape)
+    sample = {"rawdepth": depth_pred.numpy().squeeze(0).transpose(1, 2, 0),
+              "albedo": input_["albedo"].numpy().squeeze(0).transpose(1, 2, 0),
+              "mask": np.ones_like(depth_pred.numpy()).squeeze(0).transpose(1, 2, 0)}
+    depth_img_spad = simulate_spad_transform(sample)
+    print(depth_img_spad["spad"])
+    plt.figure()
+    plt.plot(depth_img_spad["spad"])
+    plt.title("Predicted depth histogram")
     plt.draw()
     plt.pause(0.001)
 
