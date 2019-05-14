Fixed numerical instability with dc, albedo, falloff turned on.

evaluate_dorn_sinkhorn_opt.py
@@ -4,7 +4,7 @@ import torch
 import json
 from torch.utils.data import DataLoader
 from utils.train_utils import init_randomness, worker_init_randomness
-from utils.eval_utils import evaluate_model_on_dataset
+from utils.eval_utils import evaluate_model_on_dataset, evaluate_model_on_data_entry
 from models.core.checkpoint import load_checkpoint, safe_makedir
 from models import make_model
 from sacred import Experiment
@@ -28,6 +28,7 @@ def cfg(data_config, spad_config):
             "lam": 1e-2,
             "kde_eps": 1e-4,
             "sinkhorn_eps": 1e-4,
+            "dc_eps": 1e-5,
             "remove_dc": spad_config["dc_count"] > 0.,
             "use_albedo": spad_config["use_albedo"],
             "use_squared_falloff": spad_config["use_squared_falloff"],
@@ -53,6 +54,7 @@ def cfg(data_config, spad_config):
     save_outputs = True
     seed = 95290421
     small_run = 0
+    entry = None
     # hyperparams = ["sgd_iters", "sinkhorn_iters", "sigma", "lam", "kde_eps", "sinkhorn_eps"]
     pdict = model_config["model_params"]
     comment = "_".join(["sgd_iters_{}".format(pdict["sgd_iters"]),
@@ -95,6 +97,7 @@ def main(model_config,
          data_config,
          seed,
          small_run,
+         entry,
          device):
     # Load the model
     model = make_model(**model_config)
@@ -107,7 +110,10 @@ def main(model_config,
     dataset = test if dataset_type == "test" else val
 
     init_randomness(seed)
-
-    print("Evaluating the model on {} ({})".format(data_config["data_name"],
-                                                   dataset_type))
-    evaluate_model_on_dataset(model, dataset, small_run, device, save_outputs, output_dir)
+    if entry is None:
+        print("Evaluating the model on {} ({})".format(data_config["data_name"],
+                                                       dataset_type))
+        evaluate_model_on_dataset(model, dataset, small_run, device, save_outputs, output_dir)
+    else:
+        print("Evaluating {}".format(entry))
+        evaluate_model_on_data_entry(model, dataset, entry, device)

models/DORN_sinkhorn_opt.py
@@ -7,9 +7,9 @@ from models.data.utils.sid_utils import SIDTorch
 from models.sinkhorn_dist import optimize_depth_map
 from models.data.utils.spad_utils import remove_dc_from_spad
 from torch.optim import SGD
-import matplotlib
-matplotlib.use("TKAgg")
-import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use("TKAgg")
+# import matplotlib.pyplot as plt
 import os
 from pdb import set_trace
 
@@ -23,7 +23,7 @@ class DORN_sinkhorn_opt:
     estimate from DORN.
     """
     def __init__(self, sgd_iters=250, sinkhorn_iters=40, sigma=2., lam=1e-2, kde_eps=1e-5,
-                 sinkhorn_eps = 1e-2,
+                 sinkhorn_eps=1e-2, dc_eps=1e-5,
                  remove_dc=True, use_albedo=True, use_squared_falloff=True,
                  lr=1e3, hints_len=68,
                  in_channels=3, in_height=257, in_width=353,
@@ -40,6 +40,7 @@ class DORN_sinkhorn_opt:
         :param lam: sinkhorn iteration parameter
         :param kde_eps: Epsilon for kernel density estimation, involved in setting floor for the kernel.
         :param sinkhorn_eps: Epsilon for sinkhorn iterations, controls convergence.
+        :param dc_eps: Epsilon for approximate lowest value of denoised histogram.
         :param remove_dc: Whether or not to remove any dc component in the spad histogram before denoising.
         :param use_albedo: Whether or not to use albedo in kernel density estimation
         :param use_squared_falloff: Whether or not to use squared falloff in kernel density estimation
@@ -64,8 +65,21 @@ class DORN_sinkhorn_opt:
         self.lam = lam
         self.kde_eps = kde_eps
         self.sinkhorn_eps = sinkhorn_eps
-
+        self.dc_eps = dc_eps
         # Define cost matrix for optimal transport problem
+        def huber(x, delta=200.):
+            if np.abs(x) < delta:
+                return 0.5 * (x ** 2)
+            return delta * (np.abs(x) - 0.5 * delta)
+
+        def berhu(x, c=5):
+            if np.abs(x) <= c:
+                return np.abs(x)
+            return (x ** 2 + c ** 2) / (2 * c)
+
+        # C = np.array([[np.abs(i - j)**2 for j in range(n_bins)] for i in range(n_bins)])
+        # C = np.array([[huber(i - j) for j in range(sid_bins)] for i in range(sid_bins)])
+        # C = np.array([[berhu(i - j) for j in range(n_bins)] for i in range(n_bins)])
         C = np.array([[(i - j)**2 for j in range(sid_bins)] for i in range(sid_bins)])
         self.cost_mat = torch.from_numpy(C).float()
 
@@ -137,24 +151,24 @@ class DORN_sinkhorn_opt:
         # DC Check
 
         denoised_spad = input_["spad"].to(device)
-        plt.figure()
-        plt.plot(denoised_spad.squeeze().clone().detach().cpu().numpy())
-        plt.title("Noisy SPAD")
-        plt.draw()
-        plt.pause(0.001)
+        # plt.figure()
+        # plt.plot(denoised_spad.squeeze().clone().detach().cpu().numpy())
+        # plt.title("Noisy SPAD")
+        # plt.draw()
+        # plt.pause(0.001)
         if self.remove_dc:
             bin_widths = (self.sid_obj.sid_bin_edges[1:] - self.sid_obj.sid_bin_edges[:-1]).cpu().numpy()
             denoised_spad = torch.from_numpy(remove_dc_from_spad(denoised_spad.squeeze(-1).squeeze(-1).cpu().numpy(),
                                                                  bin_widths)).unsqueeze(-1).unsqueeze(-1).to(device)
-            denoised_spad = denoised_spad/torch.sum(denoised_spad, dim=1, keepdim=True)
+            denoised_spad[denoised_spad < self.dc_eps] = self.dc_eps
             # print(torch.sum(denoised_spad, dim=1))
-            plt.figure()
-            plt.plot(denoised_spad.squeeze().clone().detach().cpu().numpy())
-            plt.title("Denoised SPAD")
-            plt.draw()
-            plt.pause(0.001)
+            # plt.figure()
+            # plt.plot(denoised_spad.squeeze().clone().detach().cpu().numpy())
+            # plt.title("Denoised SPAD")
+            # plt.draw()
+            # plt.pause(0.001)
         # Normalize to 1
-        denoised_spad /= torch.sum(denoised_spad)
+        denoised_spad = denoised_spad / torch.sum(denoised_spad, dim=1, keepdim=True)
         # Albedo check
         albedo = None
         if self.use_albedo:
@@ -185,11 +199,11 @@ class DORN_sinkhorn_opt:
             # # Note: align_corners=False gives same behavior as cv2.resize
             # pred = F.interpolate(depth_pred, size=original_size,
             #                      mode="bilinear", align_corners=False)
-        plt.figure()
-        plt.plot(depth_hist_final.squeeze().clone().detach().cpu().numpy())
-        plt.title("Final Histogram")
-        plt.draw()
-        plt.pause(0.001)
+        # plt.figure()
+        # plt.plot(depth_hist_final.squeeze().clone().detach().cpu().numpy())
+        # plt.title("Final Histogram")
+        # plt.draw()
+        # plt.pause(0.001)
         # compute metrics
         gt = input_["rawdepth_orig"].cpu()
         mask = input_["mask_orig"].cpu()
@@ -202,7 +216,7 @@ class DORN_sinkhorn_opt:
         # print(depth_init_map.device)
         before_metrics = self.get_metrics(depth_init_map, gt, mask)
         print("before", before_metrics)
-        input("Press enter to continue.")
+        # input("Press enter to continue.")
         return pred, metrics
 
 DORN_sinkhorn_opt.to_logprobs = staticmethod(DORN_nyu_nohints.to_logprobs)
@@ -220,9 +234,9 @@ if __name__ == "__main__":
     from collections import defaultdict
     data_config = cfg()
     spad_config = spad_cfg()
-    spad_config["dc_count"] = 0.
-    spad_config["use_albedo"] = False
-    spad_config["use_squared_falloff"] = False
+    # spad_config["dc_count"] = 0.
+    # spad_config["use_albedo"] = False
+    # spad_config["use_squared_falloff"] = False
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # print(config)
     # print(spad_config)
@@ -238,8 +252,8 @@ if __name__ == "__main__":
     dataloader = DataLoader(test, shuffle=True)
     start = perf_counter()
     init_randomness(95290421)
-    input_ = test.get_item_by_id("living_room_0059/1591")
-    for key in ["rgb", "albedo", "rawdepth", "spad", "mask", "rawdepth_orig", "mask_orig"]:
+    input_ = test.get_item_by_id("kitchen_0002/1121")
+    for key in ["rgb", "albedo", "rawdepth", "spad", "mask", "rawdepth_orig", "mask_orig", "albedo_orig"]:
         input_[key] = input_[key].unsqueeze(0)
     data_load_time = perf_counter() - start
     print("dataloader: {}".format(data_load_time))

models/sinkhorn_dist.py
@@ -62,6 +62,7 @@ def sinkhorn_dist(cost_mat, lam, hist_pred, gt_hist, num_iters=100, eps=1e-1):
     hist_pred: N x C x H x W Should all be nonzero!
     gt_hist: N x C x H x W
     """
+
     r = hist_pred.permute([0,2,3,1]).unsqueeze(-1) # N x W x H x C x 1
 #     print("r min", torch.min(r))
     c = gt_hist.permute([0,2,3,1]).unsqueeze(-1)     # N x W x H x C x 1
@@ -70,12 +71,18 @@ def sinkhorn_dist(cost_mat, lam, hist_pred, gt_hist, num_iters=100, eps=1e-1):
     x = torch.ones_like(r)/r.shape[-2] # Initialize histogram to be uniform
     K_T = K.transpose(3, 4)
     # set_trace()
+    # print(r)
+    # print(c)
+    # print(lam)
+    # print(eps)
+    # set_trace()
     for i in range(num_iters):
         temp1 = K_T.matmul(1./x) # N x W x H x C x 1
         temp2 = c*(1./temp1)  # N x W x H x C x 1
         temp3 = K.matmul(temp2)
-        r_diag = torch.diag_embed(torch.transpose(1./r, -1, -2)).squeeze(-3)
-        x_temp = r_diag.matmul(temp3)
+        # r_diag = torch.diag_embed(torch.transpose(1./r, -1, -2)).squeeze(-3)
+        # x_temp = r_diag.matmul(temp3)
+        x_temp = (1./r)*temp3
         if torch.sum(torch.abs(x - x_temp)) < eps:
             break
         # print("diff", torch.sum(torch.abs(x - x_temp))) # Inspect for convergence
@@ -92,7 +99,6 @@ def sinkhorn_dist(cost_mat, lam, hist_pred, gt_hist, num_iters=100, eps=1e-1):
     u_diag = torch.diag_embed(torch.transpose(u, -1, -2)).squeeze(-3)
     v_diag = torch.diag_embed(torch.transpose(v, -1, -2)).squeeze(-3)
     P = u_diag.matmul(K).matmul(v_diag)
-    # set_trace()
     return torch.sum(u*(K*M).matmul(v)), P
 
 
@@ -110,6 +116,7 @@ def optimize_depth_map(x_index_init, sigma, n_bins,
     x = x_index_init.clone().detach().float().requires_grad_(True)
     # print(x)
     for i in range(num_sgd_iters):
+        # with torch.autograd.detect_anomaly():
 
         # Per-pixel depth index to per-pixel histogram
         x_img = kernel_density_estimation(x, sigma, n_bins, eps=kde_eps)
@@ -125,10 +132,10 @@ def optimize_depth_map(x_index_init, sigma, n_bins,
         # print(hist_loss)
         # entropy_loss = torch.sum(entropy(x_img, dim=1))
         loss = hist_loss
-        loss.backward(retain_graph=True)
+        loss.backward()
         with torch.no_grad():
             if torch.isnan(x.grad).any().item():
-                set_trace()
+                            # set_trace()
                 print("nans detected in x.grad")
                 break
             x -= lr*x.grad
@@ -137,5 +144,5 @@ def optimize_depth_map(x_index_init, sigma, n_bins,
                 return x, x_img, x_hist
             x.grad.zero_()
             x = torch.clamp(x, min=0., max=n_bins).requires_grad_(True)
-    # print("warning: sgd exited before convergence.")
+        # print("warning: sgd exited before convergence.")
     return x, x_img, x_hist