e984ffd36c1a350c99a1f7615f6735ae36784657
Mark <nishimuramarky@yahoo.com>
2019-08-14 03:31:46 -0700

    Comparison table created.

--- paper/spad_single/sections/figures/refresh_images.sh
+++ paper/spad_single/sections/figures/refresh_images.sh
@@ -1,2 +1,3 @@
-rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/comparison_table ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
-rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/spad_example ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
+# rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/comparison_table ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
+# rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/spad_example ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
+rsync -r markn1@ares:~/spad_single/nyuv2/weighted_histogram_matching/figures/comparison ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
