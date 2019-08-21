8dc0a3a7366c0c7e0931310f0311cc46fad002b0
Mark <nishimuramarky@yahoo.com>
2019-08-20 17:31:00 -0700

    Inpainting realsense data and working on the paper figures.

--- nyuv2/realsense/matlab/nyu_utils/compile.m
+++
@@ -1,9 +0,0 @@
-libFreenectIncludeDir = '.';
-eval(sprintf('mex -I%s get_accel_data.cpp', libFreenectIncludeDir));
-
-% Use this for linux\mac.
-if ispc
-  mex mex_cbf_windows.cpp cbf_windows.cpp
-else
-  mex mex_cbf.cpp cbf.cpp
-end
