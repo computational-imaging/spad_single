43ea7f95c90a78134828996d1b5211b855fc5ea2
Mark <nishimuramarky@yahoo.com>
2019-10-07 11:50:49 -0700

    Updated teaser, moved supplement figures to gdrive.

--- paper/cvpr2020/sections/figures/refresh_images.sh
+++ paper/cvpr2020/sections/figures/refresh_images.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/comparison_table ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
 # rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/spad_example ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
 rsync -r markn1@ares:~/spad_single/nyuv2/weighted_histogram_matching/figures/comparison ~/Documents/Stanford/ares/spad_single/paper/cvpr2020/sections/figures
