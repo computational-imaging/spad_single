d204b18efe0604cab5d6c2fd73abfb85c0476d0f
Mark <nishimuramarky@yahoo.com>
2019-10-18 18:08:22 -0700

    Mark's edits.

--- paper/cvpr2020/auto/supplement.el
+++ paper/cvpr2020/auto/supplement.el
@@ -5,12 +5,12 @@
                      '(("article" "10pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8x") ("hyperref" "breaklinks=true" "bookmarks=false")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -42,6 +42,8 @@
    (TeX-add-symbols
     "cvprPaperID"
     "httilde")
+   (LaTeX-add-labels
+    "fig:sid_ablation")
    (LaTeX-add-bibliographies
     "references"))
  :latex)
--- paper/cvpr2020/sections/figures/refresh_tables.sh
+++ paper/cvpr2020/sections/figures/refresh_tables.sh
@@ -1,1 +1,1 @@
-rsync -r markn1@ares:~/spad_single/nyuv2/weighted_histogram_matching/figures/sim_table.tex ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures/sim_table.tex
+rsync -r markn1@ares:~/spad_single/nyuv2/weighted_histogram_matching/figures/sim_table.tex ~/Documents/Stanford/ares/spad_single/paper/cvpr2020/sections/figures/sim_table.tex
