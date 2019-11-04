77445325535d615e7d5effb85da51a96a5a33a19
Mark <nishimuramarky@yahoo.com>
2019-11-04 12:18:49 -0800

    Fixed results table, adjusted spacing to maintain page count.

--- paper/cvpr2020/sections/figures/refresh_images.sh
+++ paper/cvpr2020/sections/figures/refresh_images.sh
@@ -2,5 +2,6 @@
 # rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/comparison_table ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
 # rsync -r markn1@ares:~/spad_single/paper/spad_single/sections/figures/spad_example ~/Documents/Stanford/ares/spad_single/paper/spad_single/sections/figures
 rsync -r markn1@ares:~/spad_single/nyuv2/weighted_histogram_matching/figures/comparison ~/Documents/Stanford/ares/spad_single/paper/cvpr2020/sections/figures
+rsync -r markn1@ares:~/spad_single/nyuv2/weighted_histogram_matching/figures/video ~/Documents/Stanford/ares/spad_single/paper/cvpr2020/sections/figures
 rsync -r markn1@ares:~/spad_single/nyuv2/captured/figures/* ~/Documents/Stanford/ares/spad_single/paper/cvpr2020/sections/figures/captured --exclude='*.npy'
 
