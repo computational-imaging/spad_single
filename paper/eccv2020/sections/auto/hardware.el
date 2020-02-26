cebd3e9b0f9940cd9bf6a46eec983ea4dc552f3e
Mark <nishimuramarky@yahoo.com>
2020-02-26 04:05:43 -0800

    Updated with eccv format, initial revisions.

---
+++ paper/eccv2020/sections/auto/hardware.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook
+ "hardware"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:scanned_diffuse"
+    "fig:prototype"
+    "fig:results_captured"))
+ :latex)
+
---
+++ paper/eccv2020/sections/auto/method.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "method"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:pipeline"
+    "eq:pulse_integral"
+    "eq:spad_measurements"
+    "eq:ambient_estimate"
+    "eq:edge_set"
+    "eq:h_scaled"
+    "eq:sid_bin_edges"
+    "alg:ehm"))
+ :latex)
+
