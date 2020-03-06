76bbb506ea2da91c440a16632b0d58c07764f5e0
Mark <nishimuramarky@yahoo.com>
2020-03-05 18:45:43 -0800

    Changed captured_diffuse figure titles.

--- paper/eccv2020/auto/supplement.el
+++ paper/eccv2020/auto/supplement.el
@@ -5,13 +5,13 @@
                      '(("llncs" "runningheads")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "width=122mm" "left=12mm" "paperwidth=146mm" "height=193mm" "top=12mm" "paperheight=217mm") ("inputenc" "utf8x")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -54,13 +54,14 @@
     "etal"
     "ECCVSubNumber")
    (LaTeX-add-labels
+    "fig:hardware"
+    "fig:comparison"
+    "tab:photon_counts"
+    "fig:sbr_calculation"
     "fig:sid_ablation"
     "alg:ehm"
     "alg:move_pixels"
-    "fig:dither"
-    "fig:sbr_calculation"
-    "fig:comparison"
-    "tab:photon_counts")
+    "fig:dither")
    (LaTeX-add-bibliographies
     "references"))
  :latex)
